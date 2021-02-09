d39dcaa85031f0b19cf6bbef66179aac4fef548f
Michelle <labarcamoralesm@gmail.com>
2021-02-09 01:19:08 -0300

    Se añaden cambios index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import cipher from './cipher.js';
+/*import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher);*/
+
+document.getElementById('hint').onclick = function() {changeText()};
+
+function changeText() {
+  document.getElementById('hint').innerHTML = '¡HAZLO. Y si te da miedo, hazlo con miedo!';
+  document.getElementById('hint').style.color= '#f6f600';
+}
