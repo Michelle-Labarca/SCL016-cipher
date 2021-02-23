6e4a5e96642789678bd7a5a6739ea330bbf9252d
Michelle <labarcamoralesm@gmail.com>
2021-02-23 01:55:43 -0300

    Se agregan eventos en js y comentarios.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 
 console.log(cipher);*/
 
+/*Imagen onclick alerta*/
+document.getElementById('myImg').onclick = function() { alert
+('Qs tyihiw iwtivev gsqwxvymv yq pyqhs pinsv wmq pinsvev e oew tivwsqew. Gehe yqs hi qswsxvsw hifi xvefenev teve wy tvstme pinsve.')  
+}
+
+
+/* Cambio de texto*/
 document.getElementById('hint').onclick = function() {changeText()};
 
 function changeText() {
   document.getElementById('hint').innerHTML = '¡HAZLO. Y si te da miedo, hazlo con miedo!';
-  document.getElementById('hint').style.color= '#f6f600';
+  document.getElementById('hint').style.color= '#dec10c';
 }
